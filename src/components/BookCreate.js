2692e75948fc4372d587fed776d1264d09cde647
Pooja <poojasrikanth18@gmail.com>
2023-05-30 18:50:03 +0530

    final commit

--- src/components/BookCreate.js
+++ src/components/BookCreate.js
@@ -1,13 +1,18 @@
 import { useState } from "react";
-function BookCreate({onCreate}){
+import useBooksContext from "../hooks/use-books-context";
+
+
+function BookCreate(){
     const [title,setTitle] = useState('');
+    const {createBook} = useBooksContext();
+    
     const handleChange = (event)=>{
         setTitle(event.target.value);
     }
 
     const handleSubmit = (event)=>{
         event.preventDefault();
-        onCreate(title);
+        createBook(title);
         setTitle('');
     }
 
